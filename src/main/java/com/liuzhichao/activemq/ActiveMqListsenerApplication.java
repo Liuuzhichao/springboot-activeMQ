c5127cf3144d7a9e129e3e118f54f72c6d4ad035
Liuuzhichao <41028974+Liuuzhichao@users.noreply.github.com>
2019-05-30 23:25:16 +0800

    Update ActiveMqListsenerApplication.java

--- src/main/java/com/liuzhichao/activemq/ActiveMqListsenerApplication.java
+++ src/main/java/com/liuzhichao/activemq/ActiveMqListsenerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.cnpc.edw.sdp.platform.common.activemq","com.liuzhichao.activemq"})
+@ComponentScan("com.liuzhichao.activemq")
 public class ActiveMqListsenerApplication {
 
 	public static void main(String[] args) {
